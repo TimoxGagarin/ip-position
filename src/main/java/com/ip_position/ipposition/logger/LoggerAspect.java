b263d4ec947fd607b45e94ab5037d9b75c5e05af
TimoxGagarin <timarudkoooo@yandex.by>
2024-03-19 02:27:01 +0300

    minor changes 5

--- src/main/java/com/ip_position/ipposition/logger/LoggerAspect.java
+++ src/main/java/com/ip_position/ipposition/logger/LoggerAspect.java
@@ -22,23 +22,24 @@ public class LoggerAspect {
 
     @Before("execution(* com.ip_position.ipposition.services.*.*(..))")
     public void logBeforeServiceCommand(JoinPoint joinPoint) {
-        logger.info(String.format("Invoke %s with args %s", joinPoint.getSignature().getName(),
+        logger.info(() -> String.format("Invoke %s with args %s", joinPoint.getSignature().getName(),
                 Arrays.toString(joinPoint.getArgs())));
     }
 
     @AfterReturning(pointcut = "execution(* com.ip_position.ipposition.services.*.*(..))", returning = "result")
     public void logAfterServiceCommand(JoinPoint joinPoint, Object result) {
         if (result != null)
-            logger.info(String.format("Result of %s with args %s: %s", joinPoint.getSignature().getName(),
+            logger.info(() -> String.format("Result of %s with args %s: %s", joinPoint.getSignature().getName(),
                     Arrays.toString(joinPoint.getArgs()), result.toString()));
         else
-            logger.info(String.format("Result of %s with args %s: success", joinPoint.getSignature().getName(),
+            logger.info(() -> String.format("Result of %s with args %s: success", joinPoint.getSignature().getName(),
                     Arrays.toString(joinPoint.getArgs())));
     }
 
     @AfterThrowing(pointcut = "execution(* com.ip_position.ipposition.*.*.*(..))", throwing = "exception")
     public void logAfterError(JoinPoint joinPoint, Exception exception) {
-        logger.warning(String.format("Error until running %s with args %s: %s", joinPoint.getSignature().getName(),
-                Arrays.toString(joinPoint.getArgs()), exception.getMessage()));
+        logger.warning(
+                () -> String.format("Error until running %s with args %s: %s", joinPoint.getSignature().getName(),
+                        Arrays.toString(joinPoint.getArgs()), exception.getMessage()));
     }
 }
