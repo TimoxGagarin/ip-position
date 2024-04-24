466aed5264277464f2f535dc716d694ebe553595
TimoxGagarin <timarudkoooo@yandex.by>
2024-04-24 21:08:19 +0300

    fix cros 2

--- src/main/java/com/ip_position/ipposition/configs/IpInfoConfig.java
+++ src/main/java/com/ip_position/ipposition/configs/IpInfoConfig.java
@@ -3,24 +3,13 @@ package com.ip_position.ipposition.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.ip_position.ipposition.dto.IpInfoDTO;
 
 @EnableWebMvc
 @Configuration
-public class IpInfoConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+public class IpInfoConfig {
 
     @Bean
     public RestTemplate restTemplate() {
--- src/main/java/com/ip_position/ipposition/controllers/IpInfoController.java
+++ src/main/java/com/ip_position/ipposition/controllers/IpInfoController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,78 +36,82 @@ import jakarta.validation.Valid;
 @RequestMapping("ips-info")
 public class IpInfoController {
 
-        private final IpInfoService ipInfoService;
+    private final IpInfoService ipInfoService;
 
-        public IpInfoController(IpInfoService ipInfoService) {
-                this.ipInfoService = ipInfoService;
-        }
+    public IpInfoController(IpInfoService ipInfoService) {
+        this.ipInfoService = ipInfoService;
+    }
 
-        @Operation(summary = "Получить информацию об IP-адресе из внешнего API", responses = {
-                        @ApiResponse(description = "Информация об IP-адресе", content = @Content(mediaType = "application/json", schema = @Schema(implementation = IpInfo.class)))
-        })
-        @GetMapping("external_api")
-        public IpInfo getIpInfoAPI(
-                        @RequestParam(required = true) String ip) {
-                return ipInfoService.getIpInfoFromAPI(ip);
-        }
+    @Operation(summary = "Получить информацию об IP-адресе из внешнего API", responses = {
+            @ApiResponse(description = "Информация об IP-адресе", content = @Content(mediaType = "application/json", schema = @Schema(implementation = IpInfo.class)))
+    })
+    @GetMapping("external_api")
+    public IpInfo getIpInfoAPI(
+            @RequestParam(required = true) String ip) {
+        return ipInfoService.getIpInfoFromAPI(ip);
+    }
 
-        @Operation(summary = "Получить информацию об IP-адресе из базы данных по параметрам", responses = {
-                        @ApiResponse(description = "Информация об IP-адресе", content = @Content(mediaType = "application/json", schema = @Schema(implementation = IpInfo.class)))
-        })
-        @GetMapping("find")
-        public ResponseEntity<List<IpInfo>> getIpInfoDB(
-                        @Parameter(description = "IP-адрес") @RequestParam(required = false) String ip,
-                        @Parameter(description = "Широта") @RequestParam(required = false) Double latitude,
-                        @Parameter(description = "Долгота") @RequestParam(required = false) Double longitude,
-                        @Parameter(description = "ID") @RequestParam(required = false) Long id,
-                        @Parameter(description = "Часовой пояс") @RequestParam(required = false) String timeZone,
-                        @Parameter(description = "Страна") @RequestParam(required = false) String country,
-                        @Parameter(description = "Код страны") @RequestParam(required = false) String countryCode,
-                        @Parameter(description = "Регион") @RequestParam(required = false) String region,
-                        @Parameter(description = "Название региона") @RequestParam(required = false) String regionName,
-                        @Parameter(description = "Название города") @RequestParam(required = false) String cityName,
-                        @Parameter(description = "Почтовый индекс") @RequestParam(required = false) String zip,
-                        @Parameter(description = "Поставщик интернет-услуг") @RequestParam(required = false) String internetServiceProvider,
-                        @Parameter(description = "Организация") @RequestParam(required = false) String organisation,
-                        @Parameter(description = "Наименование автономной системы") @RequestParam(required = false) String autonomousSystemName) {
+    @Operation(summary = "Получить информацию об IP-адресе из базы данных по параметрам", responses = {
+            @ApiResponse(description = "Информация об IP-адресе", content = @Content(mediaType = "application/json", schema = @Schema(implementation = IpInfo.class)))
+    })
+    @CrossOrigin("/**")
+    @GetMapping("find")
+    public ResponseEntity<List<IpInfo>> getIpInfoDB(
+            @Parameter(description = "IP-адрес") @RequestParam(required = false) String ip,
+            @Parameter(description = "Широта") @RequestParam(required = false) Double latitude,
+            @Parameter(description = "Долгота") @RequestParam(required = false) Double longitude,
+            @Parameter(description = "ID") @RequestParam(required = false) Long id,
+            @Parameter(description = "Часовой пояс") @RequestParam(required = false) String timeZone,
+            @Parameter(description = "Страна") @RequestParam(required = false) String country,
+            @Parameter(description = "Код страны") @RequestParam(required = false) String countryCode,
+            @Parameter(description = "Регион") @RequestParam(required = false) String region,
+            @Parameter(description = "Название региона") @RequestParam(required = false) String regionName,
+            @Parameter(description = "Название города") @RequestParam(required = false) String cityName,
+            @Parameter(description = "Почтовый индекс") @RequestParam(required = false) String zip,
+            @Parameter(description = "Поставщик интернет-услуг") @RequestParam(required = false) String internetServiceProvider,
+            @Parameter(description = "Организация") @RequestParam(required = false) String organisation,
+            @Parameter(description = "Наименование автономной системы") @RequestParam(required = false) String autonomousSystemName) {
 
-                return ResponseEntity.ok(ipInfoService.findIpInfoFromDB(new IpInfo(
-                                id,
-                                new City(country,
-                                                countryCode,
-                                                region,
-                                                regionName,
-                                                cityName,
-                                                zip),
-                                new Position(
-                                                latitude,
-                                                longitude),
-                                timeZone,
-                                new Provider(internetServiceProvider,
-                                                organisation,
-                                                autonomousSystemName),
-                                ip)));
-        }
+        return ResponseEntity.ok(ipInfoService.findIpInfoFromDB(new IpInfo(
+                id,
+                new City(country,
+                        countryCode,
+                        region,
+                        regionName,
+                        cityName,
+                        zip),
+                new Position(
+                        latitude,
+                        longitude),
+                timeZone,
+                new Provider(internetServiceProvider,
+                        organisation,
+                        autonomousSystemName),
+                ip)));
+    }
 
-        @Operation(summary = "Добавить новую информацию об IP-адресе")
-        @PostMapping("add")
-        @ResponseStatus(HttpStatus.CREATED)
-        public void addNewIpInfo(
-                        @Parameter(description = "Информация об IP-адресе для добавления", required = true) @Valid @RequestBody List<IpInfoDTO> ipInfos) {
-                ipInfos.stream().map(ipInfo -> ipInfo.getIpInfo()).forEach(ipInfoService::addNewIpInfo);
-        }
+    @Operation(summary = "Добавить новую информацию об IP-адресе")
+    @PostMapping("add")
+    @CrossOrigin("/**")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addNewIpInfo(
+            @Parameter(description = "Информация об IP-адресе для добавления", required = true) @Valid @RequestBody List<IpInfoDTO> ipInfos) {
+        ipInfos.stream().map(ipInfo -> ipInfo.getIpInfo()).forEach(ipInfoService::addNewIpInfo);
+    }
 
-        @Operation(summary = "Удалить информацию об IP-адресе по ID")
-        @DeleteMapping("delete")
-        public void deleteIpInfo(
-                        @Parameter(description = "ID информации об IP-адресе для удаления", required = true) @RequestParam(required = true) @NonNull Long id) {
-                ipInfoService.deleteIpInfo(id);
-        }
+    @Operation(summary = "Удалить информацию об IP-адресе по ID")
+    @CrossOrigin("/**")
+    @DeleteMapping("delete")
+    public void deleteIpInfo(
+            @Parameter(description = "ID информации об IP-адресе для удаления", required = true) @RequestParam(required = true) @NonNull Long id) {
+        ipInfoService.deleteIpInfo(id);
+    }
 
-        @Operation(summary = "Обновить информацию об IP-адресе по ID")
-        @PutMapping("update")
-        public void updateIpInfo(
-                        @Parameter(description = "ID информации об IP-адресе для обновления", required = true) @RequestParam(required = true) @NonNull Long id) {
-                ipInfoService.updateIpInfo(id);
-        }
+    @Operation(summary = "Обновить информацию об IP-адресе по ID")
+    @CrossOrigin("/**")
+    @PutMapping("update")
+    public void updateIpInfo(
+            @Parameter(description = "ID информации об IP-адресе для обновления", required = true) @RequestParam(required = true) @NonNull Long id) {
+        ipInfoService.updateIpInfo(id);
+    }
 }
